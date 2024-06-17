b342149037c1572ac9275b04d50c0f5e2a443ce9
naniwakyt <naniwa.kytbb@gmail.com>
2024-06-17 11:40:40 +0900

    create calculator

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 function calculate() {
-  const a = parseFloat(document.getElementById("a").value);
-  const b = parseFloat(document.getElementById("b").value);
-  const c = parseFloat(document.getElementById("c").value);
-  const d = parseFloat(document.getElementById("d").value);
+  const tc = parseFloat(document.getElementById("tc").value);
+  const tg = parseFloat(document.getElementById("tg").value);
+  const hdlc = parseFloat(document.getElementById("hdlc").value);
+  const ldlc = parseFloat(document.getElementById("ldlc").value);
 
-  const inputs = [a, b, c, d];
-  const filledInputs = inputs.filter(input => !isNaN(input)).length;
-
-  if (filledInputs === 3) {
-      if (isNaN(a)) {
-          document.getElementById("a").value = (c + d - b).toFixed(2);
-      } else if (isNaN(b)) {
-          document.getElementById("b").value = (c + d - a).toFixed(2);
-      } else if (isNaN(c)) {
-          document.getElementById("c").value = (a + b - d).toFixed(2);
-      } else if (isNaN(d)) {
-          document.getElementById("d").value = (a + b - c).toFixed(2);
-      }
+  if (isNaN(tc)) {
+      document.getElementById("tc").value = (hdlc + ldlc + tg / 5).toFixed(2);
+  } else if (isNaN(tg)) {
+      document.getElementById("tg").value = ((tc - hdlc - ldlc) * 5).toFixed(2);
+  } else if (isNaN(hdlc)) {
+      document.getElementById("hdlc").value = (tc - ldlc - tg / 5).toFixed(2);
+  } else if (isNaN(ldlc)) {
+      document.getElementById("ldlc").value = (tc - hdlc - tg / 5).toFixed(2);
   }
 }
 
 function clearInputs() {
-  document.getElementById("a").value = "";
-  document.getElementById("b").value = "";
-  document.getElementById("c").value = "";
-  document.getElementById("d").value = "";
+  document.getElementById("tc").value = "";
+  document.getElementById("tg").value = "";
+  document.getElementById("hdlc").value = "";
+  document.getElementById("ldlc").value = "";
 }
